9271b459bdb74afea2b5851aa23593eac265c5c9
conorbradley <conorbradley49@gmail.com>
2021-11-29 22:56:59 +0000

    GUI accessing text and password fields

--- src/main/java/com/example/cs3318_a03/SignUpController.java
+++ src/main/java/com/example/cs3318_a03/SignUpController.java
@@ -2,13 +2,24 @@ package com.example.cs3318_a03;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 
 public class SignUpController {
     @FXML
     private Label welcomeText;
+    @FXML
+    private TextField email;
+    @FXML
+    private TextField password;
+
+    private String email_val;
+    private String password_val;
 
     @FXML
     protected void onSubmitButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+        email_val = email.getText();
+        password_val = password.getText();
+
+
     }
 }
